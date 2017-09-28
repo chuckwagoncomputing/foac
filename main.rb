281a719ef45cf06acf7af2f21b423143114cf382
David <dave@holdemanenterprises.com>
2017-09-28 18:49:26 -0500

    Moved main game to function

--- main.rb
+++ main.rb
@@ -93,94 +93,98 @@ else
 end
 
 if aindex = ARGV.index('-a')
- arrangement = ARGV[aindex + 1]
+ $arrangement = ARGV[aindex + 1]
 else
- arrangement = "alternating"
+ $arrangement = "alternating"
 end
 
 if sindex = ARGV.index('-1')
- strategyOne = ARGV[sindex + 1]
+ $strategyOne = ARGV[sindex + 1]
 else
- strategyOne = "random"
+ $strategyOne = "random"
 end
 if sindex = ARGV.index('-2')
- strategyTwo = ARGV[sindex + 1]
+ $strategyTwo = ARGV[sindex + 1]
 else
- strategyTwo = "random"
+ $strategyTwo = "random"
 end
 
-# make an empty chair
-$emptyChair = $playerCount + 1
+def runGame()
+ # make an empty chair
+ $emptyChair = $playerCount + 1
 
-$players = (1..$playerCount).to_a
-$pseudonyms = Hash[$players.zip($players.shuffle)]
-$positions = Hash[$players.zip($players)]
-$teams = arrange(arrangement)
+ $players = (1..$playerCount).to_a
+ $pseudonyms = Hash[$players.zip($players.shuffle)]
+ $positions = Hash[$players.zip($players)]
+ $teams = arrange($arrangement)
 
-$players.each do |player|
+ $players.each do |player|
  team = $teams[player]
  if team == 1
-  strategy = strategyOne
+  strategy = $strategyOne
  elsif team == 2
-  strategy = strategyTwo
+  strategy = $strategyTwo
  end
- eval("$player#{player} = Player.new(player, team, $pseudonyms[player], strategy)")
-end
+  eval("$player#{player} = Player.new(player, team, $pseudonyms[player], strategy)")
+ end
 
-lastPseudonym = nil
+ lastPseudonym = nil
 
-loop do
- # if the empty chair is the last before the couch, the first on the couch (position 1) is it.
- if $emptyChair == $playerCount + 1
-  callerPosition = 1
- # otherwise we can just add one to get the its position
- else
-  callerPosition = $emptyChair + 1
- end
- # get the name of the caller from their position
- callerName = $positions.key(callerPosition)
- # let them select
- calledPseudonym = eval("$player#{callerName}.think")
- # ensure the name isn't the same as was called last time
- if calledPseudonym == lastPseudonym
-  puts "Error: Cannot call the same name as the last round"
-  break
- else
-  lastPseudonym = calledPseudonym
- end
- # get the selection's real name
- calledName = $pseudonyms.key(calledPseudonym)
- # make sure they didn't call themselves
- if callerName == calledName
-  puts "Error: Cannot call yourself"
-  break
- end
- # retrieve the team they are on
- calledTeam = $teams[calledName]
- # and their position
- calledPosition = $positions[calledName]
- # move them to the empty chair
- $positions[calledName] = $emptyChair
- puts "Player #{callerName} called player #{calledName} from #{calledPosition} to #{$emptyChair} and is now #{calledPseudonym}"
- # check if someone won
- if $teams[$positions.key(1)] == $teams[$positions.key(2)]
-  if $teams[$positions.key(2)] == $teams[$positions.key(3)]
-   if $teams[$positions.key(3)] == $teams[$positions.key(4)]
-    puts "Team #{$teams[$positions.key(1)]} has won!"
-    break
+ loop do
+  # if the empty chair is the last before the couch, the first on the couch (position 1) is it.
+  if $emptyChair == $playerCount + 1
+   callerPosition = 1
+  # otherwise we can just add one to get the its position
+  else
+   callerPosition = $emptyChair + 1
+  end
+  # get the name of the caller from their position
+  callerName = $positions.key(callerPosition)
+  # let them select
+  calledPseudonym = eval("$player#{callerName}.think")
+  # ensure the name isn't the same as was called last time
+  if calledPseudonym == lastPseudonym
+   puts "Error: Cannot call the same name as the last round"
+   break
+  else
+   lastPseudonym = calledPseudonym
+  end
+  # get the selection's real name
+  calledName = $pseudonyms.key(calledPseudonym)
+  # make sure they didn't call themselves
+  if callerName == calledName
+   puts "Error: Cannot call yourself"
+   break
+  end
+  # retrieve the team they are on
+  calledTeam = $teams[calledName]
+  # and their position
+  calledPosition = $positions[calledName]
+  # move them to the empty chair
+  $positions[calledName] = $emptyChair
+  puts "Player #{callerName} called player #{calledName} from #{calledPosition} to #{$emptyChair} and is now #{calledPseudonym}"
+  # check if someone won
+  if $teams[$positions.key(1)] == $teams[$positions.key(2)]
+   if $teams[$positions.key(2)] == $teams[$positions.key(3)]
+    if $teams[$positions.key(3)] == $teams[$positions.key(4)]
+     puts "Team #{$teams[$positions.key(1)]} has won!"
+     break
+    end
    end
   end
- end
- # make the chair they were in the new empty chair
- $emptyChair = calledPosition
- # trade pseudonyms
- $pseudonyms[calledName] = $pseudonyms[callerName]
- $pseudonyms[callerName] = calledPseudonym
- # let them both figure out what we did to them
- eval("$player#{callerName}.update") 
- eval("$player#{calledName}.update")
- # let all the players take note of what happened
- $players.each do |player|
-  eval("$player#{player}.remember(calledName, calledTeam, calledPosition, calledPseudonym, callerName)")
+  # make the chair they were in the new empty chair
+  $emptyChair = calledPosition
+  # trade pseudonyms
+  $pseudonyms[calledName] = $pseudonyms[callerName]
+  $pseudonyms[callerName] = calledPseudonym
+  # let them both figure out what we did to them
+  eval("$player#{callerName}.update") 
+  eval("$player#{calledName}.update")
+  # let all the players take note of what happened
+  $players.each do |player|
+   eval("$player#{player}.remember(calledName, calledTeam, calledPosition, calledPseudonym, callerName)")
+  end
  end
 end
+
+runGame()
