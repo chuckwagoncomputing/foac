b109e7b08cd9aa7e7be9d4cf066e29695c634ab2
David <dave@holdemanenterprises.com>
2017-09-28 18:58:30 -0500

    Fixed random arrangement to get even amount on couch

--- arrangements/random.rb
+++ arrangements/random.rb
@@ -1,21 +1,41 @@
 def arrangementrandom()
  teams = Hash.new
+ teamOneCouchCount = 0
+ teamTwoCouchCount = 0
  teamOneCount = 0
  teamTwoCount = 0
  $players.each do |player|
   team = rand(2) + 1
-  if team == 1
-   if teamOneCount == $playerCount / 2
-    team = 2
+  if player < 4
+   if team == 1
+    if teamOneCouchCount == 2
+     team = 2
+    else
+     teamOneCouchCount += 1
+     teamOneCount += 1
+    end
    else
-    teamOneCount += 1
+    if teamTwoCouchCount == 2
+     team = 1
+    else
+     teamTwoCouchCount += 1
+     teamTwoCount += 1
+    end
    end
-  end
-  if team == 2
-   if teamTwoCount == $playerCount / 2
-    team = 1
-   else
-    teamTwoCount += 1
+  else
+   if team == 1
+    if teamOneCount == $playerCount / 2
+     team = 2
+    else
+     teamOneCount += 1
+    end
+   end
+   if team == 2
+    if teamTwoCount == $playerCount / 2
+     team = 1
+    else
+     teamTwoCount += 1
+    end
    end
   end
   teams[player] = team
